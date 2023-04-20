6cf9cece177e733177695f4cdcb8eca7c9803dca
9ensar8 <ensarhorozovic@gmail.com>
2023-04-20 18:18:18 +0200

    Auction app structure

---
+++ app/src/main/java/com/example/springbootreactjsauctionapp/SpringBootReactJsAuctionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactjsauctionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactJsAuctionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactJsAuctionAppApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/example/springbootreactjsauctionapp/SpringBootReactJsAuctionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactjsauctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactJsAuctionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
